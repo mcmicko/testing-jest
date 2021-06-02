f98a382a3127edd6fa30659db96cd64c04cd9323
mcmicko <mcmzrc@gmail.com>
2021-06-02 21:00:45 +0200

    adjust client index

--- client/pages/index.js
+++ client/pages/index.js
@@ -2,48 +2,44 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <div className={styles.user}>
-        <header className={styles.user__header}>
-          <img
-            src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3219/logo.svg"
-            alt=""
+    <div className={styles.user}>
+      <header className={styles.user__header}>
+        <img
+          src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/3219/logo.svg"
+          alt=""
+        />
+        <h1 className={styles.user__title}>A lightweight and simple sign-up</h1>
+      </header>
+
+      <form className={styles.form}>
+        <div className={styles.form__group}>
+          <input
+            type="text"
+            placeholder="Username"
+            className={styles.form__input}
           />
-          <h1 className={styles.user__title}>
-            A lightweight and simple sign-up
-          </h1>
-        </header>
+        </div>
 
-        <form className={styles.form}>
-          <div className={styles.form__group}>
-            <input
-              type="text"
-              placeholder="Username"
-              className={styles.form__input}
-            />
-          </div>
+        <div className={styles.form__group}>
+          <input
+            type="email"
+            placeholder="Email"
+            className={styles.form__input}
+          />
+        </div>
 
-          <div className={styles.form__group}>
-            <input
-              type="email"
-              placeholder="Email"
-              className={styles.form__input}
-            />
-          </div>
+        <div className={styles.form__group}>
+          <input
+            type="password"
+            placeholder="Password"
+            className={styles.form__input}
+          />
+        </div>
 
-          <div className={styles.form__group}>
-            <input
-              type="password"
-              placeholder="Password"
-              className={styles.form__input}
-            />
-          </div>
-
-          <button className={styles.btn} type="button">
-            Register
-          </button>
-        </form>
-      </div>
+        <button className={styles.btn} type="button">
+          Register
+        </button>
+      </form>
     </div>
   );
 }
